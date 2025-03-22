efc9ef2b4e51da5517c882231e5dcd39aba6aca3
muhammadasad878 <muhammadasad4041@gmail.com>
2025-03-23 02:27:39 +0500

    Auth for new reviews added

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -2,7 +2,7 @@ import express from "express";
 import { ExpressError, wrapAsync } from "../ExpressError.js";
 import Review from "../models/review.js";
 import { Listing } from "../models/listing.js";
-import { validateReview } from "../utils/middlewares.js";
+import { validateReview, IsLoggedIn } from "../utils/middlewares.js";
 const router = express.Router( { mergeParams:true } );
 
 
@@ -11,16 +11,17 @@ const router = express.Router( { mergeParams:true } );
 // POST Route to create reviews
 router.post(
   "/",
+  IsLoggedIn,
   validateReview,
   wrapAsync(async (req, res) => {
     const listing = await Listing.findById(req.params.id);
     if(!listing) throw new ExpressError(400, "Client Side error");
     let newReview = new Review(req.body.review);
-
+    newReview.author = req.user._id;
     listing.reviews.push(newReview);
     await listing.save();
     await newReview.save();
-
+    console.log(listing);
     res.redirect(`/listings/${listing._id}#reviews`);
   })
 );
@@ -29,6 +30,7 @@ router.post(
 // Delete Review Route
 router.delete(
   "/:reviewId",
+  IsLoggedIn,
   wrapAsync(async (req, res) => {
     let { id, reviewId } = req.params;
     if(!id || !reviewId) throw new ExpressError(400, "Client Error..");
