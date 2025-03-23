b3ba336df0d93e2627490fb02e01cfeda1d7ba35
muhammadasad878 <muhammadasad4041@gmail.com>
2025-03-23 15:59:11 +0500

    Implement Router.route for Listings

--- routes/listingRoutes.js
+++ routes/listingRoutes.js
@@ -5,22 +5,24 @@ import { IndexListings, NewListing, ShowListing, EditListingPage, UpdateListing,
 
 const router = express.Router();
 
-//  Index Route 
-router.get("/", wrapAsync(IndexListings));
+//  Index Route GET & POST Listings
+router.route("/")
+  .get(wrapAsync(IndexListings))
+  .post(IsLoggedIn, wrapAsync(NewListing));
 
-// GET Route 
+
+// GET Route render new listing form
 router.get("/new", IsLoggedIn, (req, res) => {
   res.render("listings/new.ejs");
 });
 
-// POST Route 
-router.post("/", IsLoggedIn, wrapAsync(NewListing));
 
-// Show Route to view specific listing
-router.get(
-  "/:id",
-  wrapAsync(ShowListing)
-);
+// "/:id" Routes for GET, PUT, DELETE specific listing
+router.route("/:id")
+  .get(wrapAsync(ShowListing))
+  .put(IsLoggedIn, IsOwner, wrapAsync(UpdateListing))
+  .delete(IsLoggedIn, wrapAsync(DeleteListing));
+
 
 // Edit Route 
 router.get(
@@ -30,19 +32,49 @@ router.get(
   wrapAsync(EditListingPage)
 );
 
-// PUT route to update listing
-router.put(
-  "/:id",
-  IsLoggedIn,
-  IsOwner,
-  wrapAsync(UpdateListing)
-);
 
-// Delete Route to delete specific listing
-router.delete(
-  "/:id",
-  IsLoggedIn,
-  wrapAsync(DeleteListing)
-);
+
 
 export default router;
+
+
+
+// //  Index Route 
+// router.get("/", wrapAsync(IndexListings));
+
+// // GET Route 
+// router.get("/new", IsLoggedIn, (req, res) => {
+//   res.render("listings/new.ejs");
+// });
+
+// // POST Route 
+// router.post("/", IsLoggedIn, wrapAsync(NewListing));
+
+// // Show Route to view specific listing
+// router.get(
+//   "/:id",
+//   wrapAsync(ShowListing)
+// );
+
+// // Edit Route 
+// router.get(
+//   "/:id/edit",
+//   IsLoggedIn,
+//   IsOwner,
+//   wrapAsync(EditListingPage)
+// );
+
+// // PUT route to update listing
+// router.put(
+//   "/:id",
+//   IsLoggedIn,
+//   IsOwner,
+//   wrapAsync(UpdateListing)
+// );
+
+// // Delete Route to delete specific listing
+// router.delete(
+//   "/:id",
+//   IsLoggedIn,
+//   wrapAsync(DeleteListing)
+// );
